bb63cc34504dfd5f2fc371a73e430c339a072a03
Kenny Horna <kennyhorna@gmail.com>
2017-05-08 23:26:39 -0500

    UPDATES: añadiendo librería query-builder

---
+++ config/api-query-builder.php
@@ -0,0 +1,16 @@
+<?php 
+
+return [
+    
+    'limit' => 20,
+
+    'orderBy' => [
+        [
+            'column' => 'id',
+            'direction' => 'desc'
+        ]
+    ],
+
+    'excludedParameters' => [],
+
+];
