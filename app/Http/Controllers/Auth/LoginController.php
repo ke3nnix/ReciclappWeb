80f6e85fcfe9bacb59ff9cc99cde5bfbf533fb32
Kenny Horna <kennyhorna@gmail.com>
2017-06-28 23:41:46 -0500

    ADD login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Session;
+
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -18,14 +22,16 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    use AuthenticatesUsers {
+        logout as performLogout;
+    }
 
     /**
      * Where to redirect users after login.
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -36,4 +42,43 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    // public function authenticate()
+    // {
+    //     if (Auth::attempt(['email' => $email, 'password' => $password, 'tipo' => 3])) {
+    //         // Authentication passed...
+    //         return redirect()->intended('dashboard');
+    //     }
+    // }
+
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function logout(Request $request)
+    {
+        $this->performLogout($request);
+        return redirect()->route('login');
+    }
+
+    public function authenticate(Request $request)
+    {
+       $credentials = array(
+            'email' => $request->get('email'),
+            'password' => $request->get('password'),
+            'tipo' => 3
+        );
+
+        if (Auth::attempt($credentials)) 
+        {
+            return redirect()->route('inicio');
+        }
+        else
+        {
+            Session::flash('error' , 'Credenciales erróneas y/o sin permisos suficientes');
+            return redirect()->route('login');
+        }
+    }
+
 }
