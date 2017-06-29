ccd6378d1581b349ff99209e0b02062b260aa869
Unknown <gustavo.huaracc@unmsm.edu.pe>
2017-06-28 20:17:51 -0500

    COMPOSER

--- config/app.php
+++ config/app.php
@@ -170,7 +170,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Laravel\Passport\PassportServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
+      //  Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -232,7 +232,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'Image' => Intervention\Image\ImageManagerStatic::class,
+        //'Image' => Intervention\Image\ImageManagerStatic::class,
 
     ],
 
